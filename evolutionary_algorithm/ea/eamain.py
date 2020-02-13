3ef08574908010f076e268ad77796a1f9225348b
Yean <yeanh16@gmail.com>
2020-02-13 04:10:09 +0000

    added generations and mutation

--- evolutionary_algorithm/ea/eamain.py
+++ evolutionary_algorithm/ea/eamain.py
@@ -2,19 +2,28 @@ import csv
 
 from keras.models import Sequential
 from keras.layers import Conv2D, Flatten, Dense
-from keras.optimizers import RMSprop
+from keras.optimizers import RMSprop, Adam, SGD
 from keras.initializers import RandomUniform
 from evolutionary_algorithm.ea.gym_wrapper import MainGymWrapper
 import numpy as np
 import random
 import os
 import datetime
+import copy
+
+#import multiprocessing as mp
+
 from time import sleep
 
 import gym
 
-POPULATION_SIZE = 1
+#pool = mp.Pool(mp.cpu_count())
+##Make sure selection rate and elite ratio produce integers from population size
+POPULATION_SIZE = 100
 SELECTION_RATE = 0.1
+MUTATION_POWER = 0.02
+ELITE_RATIO = 0.02
+NUMBEROFGENERATIONS = 1000
 FRAMES_IN_OBSERVATION = 4
 FRAME_SIZE = 84
 INPUT_SHAPE = (FRAMES_IN_OBSERVATION, FRAME_SIZE, FRAME_SIZE)
@@ -76,9 +85,18 @@ class ConvolutionalNeuralNetwork:
         #print("predict " + str(np.argmax(q_values[0])))
         return np.argmax(q_values[0])
 
+    def set_weights(self, weights):
+        self.model.set_weights(weights)
+
+    def get_weights(self):
+        return self.model.get_weights()
+
     def save_model(self):
         self.model.save(MODEL_FILEPATH)
-        del self.model
+        #del self.model
+
+
+
 
 
 class EA:
@@ -86,6 +104,8 @@ class EA:
         self.env_name = env_name
         self.pop_size = pop_size
         self.env = MainGymWrapper.wrap(gym.make(self.env_name))
+        #for i in range(0, mp.cpu_count()):
+        #    self.envs.append(MainGymWrapper.wrap(gym.make(self.env_name)))
         self.input_shape = input_shape
         self.pop = self._intialise_pop()
         self.selection_rate = selection_rate
@@ -99,39 +119,77 @@ class EA:
         return pop
 
     def train_evolutionary_algorithm(self):
-        pop_fitness = []
-        cumulative_fitness = 0
-        min_gen_fitness = 9999
-        max_gen_fitness = -9999
         best_fitness = -9999
-        # model = ConvolutionalNeuralNetwork(INPUT_SHAPE, env.action_space.n)
-        count = 0
-        for model in self.pop:
-            count += 1
-            episode_reward = self._fitness_test(model)
-            print("Chromosome " + str(count) + " reward: " + str(episode_reward))
-            if episode_reward < min_gen_fitness:
-                min_gen_fitness = episode_reward
-            if episode_reward > max_gen_fitness:
-                max_gen_fitness = episode_reward
-            cumulative_fitness += episode_reward
-            pop_fitness.append((model, episode_reward))
-        av_gen_fitness = cumulative_fitness / self.pop_size
-        pop_fitness.sort(key=lambda x: x[1])
-        print("Generation min fitness: " + str(min_gen_fitness)
-              + " max_fitness: " + str(max_gen_fitness)
-              + " av_fitness: " + str(av_gen_fitness))
-        self.logger.log_line_csv([min_gen_fitness] + [max_gen_fitness] + [av_gen_fitness] + [cumulative_frames])
-        if pop_fitness[-1][1] > best_fitness:
-            print("Generation max fitness increase, saving model...")
-            best_fitness = pop_fitness[-1][1]
-            pop_fitness[-1][0].save_model()  # save top model
-        # print("Population fitness: " + str(pop_fitness))
+        for g in range(0, NUMBEROFGENERATIONS):
+            print("Generation " + str(g))
+            pop_fitness = []
+            cumulative_fitness = 0
+            min_gen_fitness = 9999
+            max_gen_fitness = -9999
+            cumulative_frames = 0
+            # model = ConvolutionalNeuralNetwork(INPUT_SHAPE, env.action_space.n)
+            count = 0
+            # fitness_result_objects = [pool.apply_async(self._fitness_test, args=(model, env)) for model in self.pop for env in self.envs]
+            for model in self.pop:
+                count += 1
+                _, episode_reward, frames = self._fitness_test(model)
+                cumulative_frames += frames
+                print("Chromosome " + str(count) + " reward: " + str(episode_reward))
+                if episode_reward < min_gen_fitness:
+                    min_gen_fitness = episode_reward
+                if episode_reward > max_gen_fitness:
+                    max_gen_fitness = episode_reward
+                cumulative_fitness += episode_reward
+                pop_fitness.append((model, episode_reward))
+            av_gen_fitness = cumulative_fitness / self.pop_size
+            pop_fitness.sort(key=lambda x: x[1])
+            print("Generation min fitness: " + str(min_gen_fitness)
+                  + " max_fitness: " + str(max_gen_fitness)
+                  + " av_fitness: " + str(av_gen_fitness))
+            self.logger.log_line_csv([min_gen_fitness] + [max_gen_fitness] + [av_gen_fitness] + [cumulative_frames])
+            if pop_fitness[-1][1] > best_fitness:
+                print("Generation max fitness increase, saving model...")
+                best_fitness = pop_fitness[-1][1]
+                pop_fitness[-1][0].save_model()  # save top model
+            print("Population fitness: " + str(pop_fitness))
+
+            # add elites
+            new_pop = []
+            selected = []
+            number_of_elites = int(POPULATION_SIZE * ELITE_RATIO)
+            for i in range(POPULATION_SIZE-number_of_elites, POPULATION_SIZE):
+                selected.append(pop_fitness[i])
+                new_pop.append(pop_fitness[i][0])
+
+            # selection, fill up rest of selection list
+            begin_index = int(POPULATION_SIZE -(POPULATION_SIZE*SELECTION_RATE))
+            for i in range(begin_index, POPULATION_SIZE-number_of_elites):
+                selected.append(pop_fitness[i])
+
+            # mutation
+            while len(new_pop) < POPULATION_SIZE:
+                new_model = copy.deepcopy(selected[np.random.randint(0, int(POPULATION_SIZE*SELECTION_RATE))][0])
+                new_model = self._mutation(new_model)
+                new_pop.append(new_model)
+
+            #next generation
+            self.pop = new_pop
+
+
+    # results = [r.get()[1] for r in fitness_result_objects]
+    # pool.close
+    # pool.join
+    # print(fitness_result_objects)
+
+    def _mutation(self, model):
+        # add random gaussian noise to every weight
+        print("weights before mutation: " + str(np.array(model.get_weights)))
+        new_weights = [w + np.random.normal(0, MUTATION_POWER) for w in model.get_weights()]
+        model.set_weights(new_weights)
+        print("weights after mutation " + str(np.array(model.get_weights)))
+        return model
 
     def _selection(self):
-        pass
-
-    def _mutation(self):
         pass
 
     def _crossover(self):
@@ -141,14 +199,18 @@ class EA:
         state = self.env.reset()
         terminated = False
         episode_reward = 0
+        frames_count = 0
         while not terminated:
             action = model.predict(state)
             state, reward, terminated, info = self.env.step(action)
-            self.cumulative_frames += 1
+            frames_count += 1
             # env.render()
             episode_reward += reward
             # sleep(0.01)
-        return episode_reward
+        #print(str(episode_reward))
+        return model, episode_reward, frames_count
+
+
 
 class Logger:
     def __init__(self, logpath):
@@ -160,6 +222,8 @@ class Logger:
         scores_file = open(self.logpath, "a")
         with scores_file:
             writer = csv.writer(scores_file)
+            writer.writerow(["POPULATION_SIZE", "SELECTION_RATE", "MUTATION_POWER", "ELITE_RATIO"])
+            writer.writerow([POPULATION_SIZE, SELECTION_RATE, MUTATION_POWER, ELITE_RATIO])
             writer.writerow(["min", "max", "av", "frames"])
         ##/write header
 
@@ -177,7 +241,29 @@ class Logger:
 
 
 def __main__():
+    #pass
     run = EA(ENV_NAME, POPULATION_SIZE, INPUT_SHAPE, SELECTION_RATE)
     run.train_evolutionary_algorithm()
 
 __main__()
+
+# test_model = Sequential()
+# test_model.add(Dense(2, activation='relu', use_bias=False, kernel_initializer='ones', input_shape=(1,)))
+# test_model.add(Dense(2, activation='relu', use_bias=False, kernel_initializer='ones'))
+# test_model.add(Dense(2, use_bias=False, kernel_initializer='ones'))
+#
+# test_model.compile(loss='mean_squared_error', optimizer=SGD(learning_rate=0.01, momentum=0.0, nesterov=False))
+# test_model.summary()
+#
+# test_weights = np.array(test_model.get_weights())
+# for x in np.nditer(test_weights, op_flags=['readwrite'], flags=['refs_ok']):
+#     x[...]
+#
+# weights = [w + 2 for w in test_model.get_weights()]
+# print(weights)
+#
+# print("test weights before: \n" + str(test_weights))
+#
+# test_model.set_weights(weights)
+#
+# print("test weights: \n" + str(test_weights))
